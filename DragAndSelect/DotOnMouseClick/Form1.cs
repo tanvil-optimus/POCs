97fca61bc7e7943c2aaf9e3d6a52bd22de5cb24c
tanvi.lugani <tanvi.lugani@optimusinfo.com>
2016-11-02 13:37:33 +0530

    POC : 1. Drag Drop 2. Select functionality

---
+++ DragAndSelect/DotOnMouseClick/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DotOnMouseClick
+{
+    public partial class Form1 : Form
+    {
+        private Point _circleStartPoint;
+        private Rectangle _rect;
+        private int _pixelSize = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+                return;
+
+            _circleStartPoint = e.Location;
+            _pixelSize = 2; //will be set to one later. This is for illustration purpose.
+
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            //// Draw the rectangle...
+            //if (pictureBox1.Image != null) //Ideally this condition should be added.
+            //{
+            Brush brush = new SolidBrush(Color.White);
+                e.Graphics.FillRectangle(brush,_circleStartPoint.X, _circleStartPoint.Y,2,2);
+            //}
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            _circleStartPoint.X = 0;
+            _circleStartPoint.Y = 0;
+            _pixelSize = 0;
+
+            pictureBox1.Invalidate();
+        }
+
+        
+    }
+}
---
+++ DragAndSelect/DragAndSelect/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DragAndSelect
+{
+    public partial class Form1 : Form
+    {
+
+        private Point _rectStartPoint;
+        private Rectangle _rect;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Start the rectangle
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _rectStartPoint = e.Location;
+            Invalidate();
+        }
+
+        //Draw the rectangle
+        private void pictureBox1_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+                return;
+            Point tempEndPoint = e.Location;
+            _rect.Location = new Point(
+                Math.Min(_rectStartPoint.X, tempEndPoint.X),
+                Math.Min(_rectStartPoint.Y, tempEndPoint.Y));
+            _rect.Size = new Size(
+                Math.Abs(_rectStartPoint.X - tempEndPoint.X),
+                Math.Abs(_rectStartPoint.Y - tempEndPoint.Y));
+            pictureBox1.Invalidate();
+        }
+
+        // Draw Area
+        //
+        private void pictureBox1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            // Draw the rectangle...
+            if (pictureBox1.Image != null)
+            {
+                Brush brush = new SolidBrush(Color.FromArgb(255,0,0,0));
+                e.Graphics.FillRectangle(brush, _rect);
+            }
+        }
+
+        //Hide the rectangle.
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                _rect.Location = new Point(
+              Math.Min(0,0),
+              Math.Min(0, 0));
+                _rect.Size = new Size(
+                    Math.Abs(0),
+                    Math.Abs(0));
+                pictureBox1.Invalidate();
+            }
+        }
+ 
+    }
+}
