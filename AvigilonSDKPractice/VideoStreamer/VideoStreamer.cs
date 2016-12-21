cecb7da131eed3dc3dc7eacf9ba6bb888eff4fe0
Tanvi Lugani <tanvi.lugani@optimusinfo.com>
2016-12-21 11:22:11 +0530

    Added files for VideoStreamer project

---
+++ AvigilonSDKPractice/VideoStreamer/VideoStreamer.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Net;
+namespace VideoStreamer
+{
+    public class VideoStreamer
+    {
+        private AvigilonDotNet.AvigilonSdk _avigilonSdk;
+
+        public VideoStreamer()
+        {
+            _avigilonSdk = new AvigilonDotNet.AvigilonSdk();
+        }
+
+        public AvigilonDotNet.IAvigilonControlCenter CreateInstance(bool isAutoDiscoverChecked)
+        {
+            AvigilonDotNet.SdkInitParams initParams = new AvigilonDotNet.SdkInitParams(
+                    AvigilonDotNet.AvigilonSdk.MajorVersion,
+                    AvigilonDotNet.AvigilonSdk.MinorVersion);
+            initParams.AutoDiscoverNvrs = isAutoDiscoverChecked;
+            initParams.ServiceMode = false;
+
+            return _avigilonSdk.CreateInstance(initParams);
+        }
+
+        public AvigilonDotNet.AvgError AddSite(ref AvigilonDotNet.IAvigilonControlCenter controlCenter, string ip, int port)
+        {
+            IPAddress ipAddress;
+            IPAddress.TryParse(ip, out ipAddress);
+            var ipEndPoint = new IPEndPoint(ipAddress, port);
+            return controlCenter.AddNvr(ipEndPoint);
+        }
+
+        public AvigilonDotNet.INvr GetNvr(AvigilonDotNet.IAvigilonControlCenter controlCenter, IPAddress ip)
+        {
+            return controlCenter.GetNvr(ip);
+        }
+    }
+}
